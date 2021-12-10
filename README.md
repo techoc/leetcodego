3f08a8134e8caa285e505f73c320b6410a75eb3e
techoc <techoc@foxmail.com>
2021-12-11 00:08:24 +0800

    748. 最短补全词

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## LeetCode的Go语言刷题记录
 
-
 ## 简单
+
 - [1.两数之和](leetcode/1.go)
 - [53. 最大子序和](leetcode/53.go)
 - [88.合并两个有序数组](leetcode/88.go)
@@ -11,8 +11,10 @@
 - [350.两个数组的交集 II](leetcode/350.go)
 - [387.字符串中的第一个唯一字符](leetcode/387.go)
 - [566.重塑矩阵](leetcode/556.go)
+- [748.最短补全词]()
 
 ## 中等
+
 - [36.有效的数独](leetcode/36.go)
 - [73.矩阵置零](leetcode/73.go)
 
---
+++ leetcode/748.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import "unicode"
+
+//	统计 licensePlate 中每个字母的出现次数（忽略大小写）
+//	然后遍历 words 中的每个单词
+//	若 26 个字母在该单词中的出现次数均不小于在 licensePlate 中的出现次数，则该单词是一个补全词
+//	返回最短且最靠前的补全词
+func ShortestCompletingWord(licensePlate string, words []string) (ans string) {
+	cnt := [26]int{}
+	for _, ch := range licensePlate {
+		if unicode.IsLetter(ch) {
+			cnt[unicode.ToLower(ch)-'a']++
+		}
+	}
+
+next:
+	for _, word := range words {
+		c := [26]int{}
+		for _, ch := range word {
+			c[ch-'a']++
+		}
+		for i := 0; i < 26; i++ {
+			if c[i] < cnt[i] {
+				continue next
+			}
+		}
+		if ans == "" || len(word) < len(ans) {
+			ans = word
+		}
+	}
+	return
+}
