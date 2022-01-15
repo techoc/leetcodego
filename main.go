2f0e5868ea80f695db83097d35647380ad2081cc
techoc <techoc@foxmail.com>
2022-01-15 21:20:08 +0800

    1716.计算力扣银行的钱

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/techoc/leetcodego/leetcode"
 )
 
 func main() {
-	nums := []int{7, 1, 5, 3, 6, 4}
-	// nums := []int{-2}
-	fmt.Println(leetcode.MaxProfit(nums))
-	fmt.Println(int(^uint(0) >> 1))
+	leetcode.TotalMoney(14)
 }
