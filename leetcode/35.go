f6f33105816b3deb6678d3a4d7539ae8b6704e18
techoc <techoc@foxmail.com>
2022-02-23 09:55:02 +0800

    35.搜索插入位置(二分法)

--- leetcode/35.go
+++ leetcode/35.go
@@ -9,6 +9,7 @@ package leetcode
 //输入: nums = [1,3,5,6], target = 2
 //输出: 1
 
+//普通遍历 O(n)
 func searchInsert(nums []int, target int) int {
 	for index, value := range nums {
 		if value == target {
@@ -20,4 +21,22 @@ func searchInsert(nums []int, target int) int {
 	}
 	//如果没有找到，则返回最后一个元素的下标
 	return len(nums)
+}
+
+//二分查找 O(log n)
+func searchInsertQuick(nums []int, target int) int {
+	//初始化左右指针
+	left, right := 0, len(nums)-1
+	ans := len(nums)
+	for left <= right {
+		//防止数字大小溢出，等同于(right+left)/2
+		mid := (right-left)>>1 + left
+		if target <= nums[mid] {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return ans
 }
