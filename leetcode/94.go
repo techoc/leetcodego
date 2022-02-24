9f890e5a88f9101771ddce9b603079d2062da37c
techoc <techoc@foxmail.com>
2022-02-24 11:16:24 +0800

    94.二叉树的中序遍历

---
+++ leetcode/94.go
@@ -0,0 +1,14 @@
+package leetcode
+
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+	res = append(res, inorderTraversal(root.Left)...)
+	res = append(res, root.Val)
+	res = append(res, inorderTraversal(root.Right)...)
+	return res
+}
