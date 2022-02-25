ce2a525ddaa0fc765eae89fd3fa79f62179cabda
techoc <techoc@foxmail.com>
2022-02-25 09:50:18 +0800

    537.复数乘法

---
+++ leetcode/537.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//复数 可以用字符串表示，遵循 "实部+虚部i" 的形式，并满足下述条件：
+//实部 是一个整数，取值范围是 [-100, 100]
+//虚部 也是一个整数，取值范围是 [-100, 100]
+//i2 == -1
+//给你两个字符串表示的复数 num1 和 num2 ，请你遵循复数表示形式，返回表示它们乘积的字符串。
+//示例 1：
+//输入：num1 = "1+1i", num2 = "1+1i"
+//输出："0+2i"
+//解释：(1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i ，你需要将它转换为 0+2i 的形式。
+
+func parseComplexNumber(num string) (real, imag int) {
+	i := strings.IndexByte(num, '+')
+	real, _ = strconv.Atoi(num[:i])
+	imag, _ = strconv.Atoi(num[i+1 : len(num)-1])
+	return
+}
+
+func complexNumberMultiply(num1, num2 string) string {
+	//得到实部和虚部
+	real1, imag1 := parseComplexNumber(num1)
+	real2, imag2 := parseComplexNumber(num2)
+	return fmt.Sprintf("%d+%di", real1*real2-imag1*imag2, real1*imag2+imag1*real2)
+}
