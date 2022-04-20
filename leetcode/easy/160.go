5ba2516e2402c532f339af470b8131f2a7fb6fbf
techoc <techoc@foxmail.com>
2022-04-20 17:35:12 +0800

    :sparkles:160.相交链表

---
+++ leetcode/easy/160.go
@@ -0,0 +1,26 @@
+package easy
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	pa, pb := headA, headB
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+	return pa
+}
