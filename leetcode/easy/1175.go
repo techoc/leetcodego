4e65615d70249300a35354e52d505e77e895f6e8
Rui Yang <techoc@foxmail.com>
2022-06-30 22:33:02 +0800

    :sparkles:1175. 质数排列

---
+++ leetcode/easy/1175.go
@@ -0,0 +1,31 @@
+package easy
+
+const mod int = 1e9 + 7
+
+func numPrimeArrangements(n int) int {
+	numPrimes := 0
+	for i := 2; i <= n; i++ {
+		//如果是质数则加一
+		if isPrime(i) {
+			numPrimes++
+		}
+	}
+	return factorial(numPrimes) * factorial(n-numPrimes) % mod
+}
+
+func isPrime(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func factorial(n int) int {
+	f := 1
+	for i := 1; i <= n; i++ {
+		f = f * i % mod
+	}
+	return f
+}
