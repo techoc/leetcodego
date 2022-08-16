6204eaccbdc2439470e3c9fe10ba2e9c1c01d91a
techoc <techoc@foxmail.com>
2022-08-16 22:55:39 +0800

    :sparkles:1656.设计有序流

---
+++ leetcode/easy/1656.go
@@ -0,0 +1,20 @@
+package easy
+
+type OrderedStream struct {
+	stream []string
+	ptr    int
+}
+
+func Constructor(n int) OrderedStream {
+	//构造
+	return OrderedStream{make([]string, n+1), 1}
+}
+
+func (s *OrderedStream) Insert(idKey int, value string) []string {
+	s.stream[idKey] = value
+	start := s.ptr
+	for s.ptr < len(s.stream) && s.stream[s.ptr] != "" {
+		s.ptr++
+	}
+	return s.stream[start:s.ptr]
+}
