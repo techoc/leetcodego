5cc1ff770e9c22cc46806e97afe47a2911eadd63
techoc <techoc@foxmail.com>
2022-08-25 18:23:19 +0800

    :sparkles:658.找到 K 个最接近的元素

--- README.md
+++ README.md
@@ -108,6 +108,7 @@
 - [540.有序数组中的单一元素](leetcode/medium/540.go)
 - [654.最大二叉树](leetcode/medium/654.go)
 - [655.输出二叉树](leetcode/medium/655.go)
+- [658.找到 K 个最接近的元素](leetcode/medium/658.go)
 - [688.骑士在棋盘上的概率](leetcode/medium/688.go)
 - [720.词典中最长的单词](leetcode/medium/720.go)
 - [763.划分字母区间](leetcode/medium/763.go)
---
+++ leetcode/medium/658.go
@@ -0,0 +1,19 @@
+package medium
+
+import "sort"
+
+func findClosestElements(arr []int, k, x int) []int {
+	//找到x在数组中的位置
+	right := sort.SearchInts(arr, x)
+	left := right - 1
+	for ; k > 0; k-- {
+		if left < 0 {
+			right++
+		} else if right >= len(arr) || x-arr[left] <= arr[right]-x {
+			left--
+		} else {
+			right++
+		}
+	}
+	return arr[left+1 : right]
+}
