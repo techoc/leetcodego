6678067726baec572984dac2e03d8bea74052c82
techoc <techoc@foxmail.com>
2022-09-16 21:09:01 +0800

    :sparkles:219.存在重复元素 II

---
+++ leetcode/easy/219.go
@@ -0,0 +1,16 @@
+package easy
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	n := len(nums)
+	set := map[int]bool{}
+	for i := 0; i < n; i++ {
+		if i > k {
+			set[nums[i-k-1]] = false
+		}
+		if set[nums[i]] {
+			return true
+		}
+		set[nums[i]] = true
+	}
+	return false
+}
