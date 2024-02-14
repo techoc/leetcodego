ff462858c1f6e21f4da44ee81f1bb75e0ac8cd5c
techoc <techoc@foxmail.com>
2024-02-14 22:38:54 +0800

    :sparkles:145.二叉树的后序遍历

--- leetcode/utils/list_node.go
+++ leetcode/utils/list_node.go
@@ -6,18 +6,18 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func listToTree(arr []any) *TreeNode {
-	return listToTreeDFS(arr, 0)
+func ListToTree(arr []any) *TreeNode {
+	return ListToTreeDFS(arr, 0)
 }
 
-func listToTreeDFS(arr []any, i int) *TreeNode {
+func ListToTreeDFS(arr []any, i int) *TreeNode {
 	if i < 0 || i >= len(arr) || arr[i] == nil {
 		return nil
 	}
 	root := &TreeNode{
 		Val: arr[i].(int),
 	}
-	root.Left = listToTreeDFS(arr, 2*i+1)
-	root.Right = listToTreeDFS(arr, 2*i+2)
+	root.Left = ListToTreeDFS(arr, 2*i+1)
+	root.Right = ListToTreeDFS(arr, 2*i+2)
 	return root
 }
